0caccb543a59fe6cd4f41abd1ba7de5c5f0aa12c
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2025-02-06 22:47:04 -0500

    Actualizaciones

---
+++ src/main/java/espcoh/ec/edu/gestion1/Controlador/Controlador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package espcoh.ec.edu.gestion1.Controlador;
+
+/**
+ *
+ * @author user
+ */
+public class Controlador {
+    
+    
+    
+    
+    
+}
